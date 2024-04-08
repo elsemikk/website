60c7f23d4ebf31d7e30ce92fa4d613d7677c07f8
Uriel Garcilazo <garcilazo.uriel@gmail.com>
2024-04-08 18:23:52 -0400

    test2_08_04_2024

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Set up routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public','index.html'));
+    res.sendFile(path.join(__dirname, 'elsemikkelsendotcom','index.html'));
 });
 
 // Start the server
